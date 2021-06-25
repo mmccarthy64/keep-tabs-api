97272a4f49cda4000ce4140f51acff2100245c62
mmccarthy64 <mmccarthy64@hotmail.com>
2021-06-25 12:56:44 -0500

    add book seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Book.create(title: "Alexander Hamilton", author: "Ron Chernow", page_count: 570, img: "https://images-na.ssl-images-amazon.com/images/I/9168wNMBk1L.jpg")
+Book.create(title: "The New Right", author: "Michael Malice", page_count: 280, img: "https://images-na.ssl-images-amazon.com/images/I/510YXq4hZFL._SY291_BO1,204,203,200_QL40_ML2_.jpg")
+Book.create(title: "Starting Strength", author: "Mark Rippetoe", page_count: 600, img: "https://m.media-amazon.com/images/I/51qninUnshL.jpg")
+Book.create(title: "The Fountainhead", author: "Ayn Rand", page_count: 800, img: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/The_Fountain_Head_%281943_1st_ed%29_-_Ayn_Rand.jpg/220px-The_Fountain_Head_%281943_1st_ed%29_-_Ayn_Rand.jpg")
+Book.create(title: "KJV Bible", author: "God", page_count: 1800, img: "https://www.christianartgifts.com/product-images/KJV028_1--front.jpg?resizeid=5&resizeh=1200&resizew=1200")
+Book.create(title: "Theory and History", author: "Ludwig von Mises", page_count: 450, img: "https://upload.wikimedia.org/wikipedia/en/thumb/0/0d/HumanActionFirst.jpg/220px-HumanActionFirst.jpg")
+Book.create(title: "Modern Times", author: "Paul Johnson", page_count: 450, img: "https://upload.wikimedia.org/wikipedia/en/thumb/a/a1/Modern_Times_%28book%29.jpg/220px-Modern_Times_%28book%29.jpg")
+Book.create(title: "Man, Economy, and State", author: "Murray N. Rothbard", page_count: 450, img: "https://m.media-amazon.com/images/I/51Em01CeumL.jpg")
