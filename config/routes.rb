24800483887728f42b6faeea523822166ab9130b
mmccarthy64 <mmccarthy64@hotmail.com>
2021-06-25 14:06:21 -0500

    Add RESTful routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :books
+  # resources :comments
+  resources :books do 
+    resources :comments, only: [:new, :create, :index, :show]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
