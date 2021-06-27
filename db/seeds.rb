372f6205c915483b8fa08eb20a09cca07648817f
mmccarthy64 <mmccarthy64@hotmail.com>
2021-06-27 13:59:06 -0500

    Update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,9 +11,10 @@ Book.create(title: "The New Right", author: "Michael Malice", page_count: 280, i
 Book.create(title: "Starting Strength", author: "Mark Rippetoe", page_count: 600, image: "https://m.media-amazon.com/images/I/51qninUnshL.jpg")
 Book.create(title: "The Fountainhead", author: "Ayn Rand", page_count: 800, image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/The_Fountain_Head_%281943_1st_ed%29_-_Ayn_Rand.jpg/220px-The_Fountain_Head_%281943_1st_ed%29_-_Ayn_Rand.jpg")
 Book.create(title: "KJV Bible", author: "God", page_count: 1800, image: "https://www.christianartgifts.com/product-images/KJV028_1--front.jpg?resizeid=5&resizeh=1200&resizew=1200")
-Book.create(title: "Theory and History", author: "Ludwig von Mises", page_count: 450, image: "https://upload.wikimedia.org/wikipedia/en/thumb/0/0d/HumanActionFirst.jpg/220px-HumanActionFirst.jpg")
+Book.create(title: "Human Action", author: "Ludwig von Mises", page_count: 450, image: "https://upload.wikimedia.org/wikipedia/en/thumb/0/0d/HumanActionFirst.jpg/220px-HumanActionFirst.jpg")
 Book.create(title: "Modern Times", author: "Paul Johnson", page_count: 450, image: "https://upload.wikimedia.org/wikipedia/en/thumb/a/a1/Modern_Times_%28book%29.jpg/220px-Modern_Times_%28book%29.jpg")
 Book.create(title: "Man, Economy, and State", author: "Murray N. Rothbard", page_count: 450, image: "https://m.media-amazon.com/images/I/51Em01CeumL.jpg")
+Book.create(title: "The Hitchhiker's Guide to the Galaxy", author: "Douglas Adams", page_count: 500, image: "https://i.guim.co.uk/img/static/sys-images/Guardian/Pix/pictures/2015/6/25/1435245979235/047c9878-9845-473c-9635-5f32545746b0-1355x2040.jpeg?width=700&quality=85&auto=format&fit=max&s=606433bda33c8c27c5ebd7ba85900473" )
 
 Comment.create(content: "Good read", book: Book.find_by(title: "KJV Bible"))
 Comment.create(content: "I prefer the ESV", book: Book.find_by(title: "KJV Bible"))
