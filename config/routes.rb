796c32ab6d8950bbc1b8bf95e7eb3eae5d6179af
mmccarthy64 <mmccarthy64@hotmail.com>
2021-07-05 10:07:23 -0500

    add registration create route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :sessions, only: [:create]
+  resources :registrations, only: [:create]
   root to: 'static#home'
   resources :books do 
     resources :comments, only: [:new, :create, :index, :show]
