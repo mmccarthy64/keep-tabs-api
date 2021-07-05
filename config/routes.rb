f0aba4b69c69d5a9fd6a0399e36152f65e1740ad
mmccarthy64 <mmccarthy64@hotmail.com>
2021-07-05 09:52:38 -0500

    Create static controller, route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  # resources :comments
+  root to: 'static#home'
   resources :books do 
     resources :comments, only: [:new, :create, :index, :show]
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
