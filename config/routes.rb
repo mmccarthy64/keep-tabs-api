154ba9129846d818d5def342428edcb91be2c0f6
mmccarthy64 <mmccarthy64@hotmail.com>
2021-07-05 10:04:38 -0500

    create session route, controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :sessions, only: [:create]
   root to: 'static#home'
   resources :books do 
     resources :comments, only: [:new, :create, :index, :show]
