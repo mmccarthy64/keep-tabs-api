224ce388685f34d1e2cb88a1cd52cf969459b7c9
mmccarthy64 <mmccarthy64@hotmail.com>
2021-07-08 10:48:35 -0500

    Create migration user_id to books, update seed data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_08_151803) do
+ActiveRecord::Schema.define(version: 2021_07_08_152924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2021_07_08_151803) do
     t.string "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "user_id"
+    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_books_on_user_id"
   end
 
