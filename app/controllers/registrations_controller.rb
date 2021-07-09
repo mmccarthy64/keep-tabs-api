c68866b1474694700dce29c2850ca4afdff3e8b6
mmccarthy64 <mmccarthy64@hotmail.com>
2021-07-08 22:10:23 -0500

    add check for username uniqueness

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,12 +1,12 @@
 class RegistrationsController < ApplicationController
     def create
-        user = User.create(
+        user = User.new(
             username: params['user']['username'],
             password: params['user']['password'],
             password_confirmation: params['user']['password_confirmation']
         )
 
-        if user
+        if user.save
             session[:user_id] = user.id
             # binding.pry
             render json: {
